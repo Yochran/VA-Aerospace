f84b54cf4099a886a2fb5ab20ef78a653f726f5a
Yochran <71285258+Yochran@users.noreply.github.com>
2021-02-26 18:38:47 -0500

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,26 @@
+// Functions
+let loadPage = function() {
+    setTimeout(() => {
+        document.getElementById("loading").style.opacity = 0 + "%";
+        setTimeout(() => {
+            document.getElementById("everything").style.opacity = 100 + "%";
+        }, 500);
+    }, 1500);
+}
+
+function parallax() {
+    document.getElementById("background").style.top = Math.floor(-15 - 0.045 * window.scrollY) + "%";
+    document.getElementById("backsheet").style.top = Math.floor(100 + 0.001 * window.scrollY) + "%";
+    document.getElementById("separator").style.top = Math.floor(97.5 + 0.0001 * window.scrollY) + "%";
+}
+
+// Events
+document.onscroll = () => {
+    parallax();
+};
+document.onreadystatechange = () => {
+    loadPage().then(() => {
+        delete loadPage();
+        loadPage = "";
+    })
+};
